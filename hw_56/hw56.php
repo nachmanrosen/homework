731a19dee11f7e1b398f33962756b297898e798c
Nachman Rosen <nachmanrosen@gmail.com>
2017-06-15 21:43:00 -0400

    hw_56

---
+++ hw_56/hw56.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>php</title>
+ <link href="/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+
+
+<?php 
+$p= "Donald Trump ";
+$y= "2017";
+echo $p,$y;
+?>
+<br/>
+<h1>Presidents of US</h1>
+<table class="table table-striped">
+    <tr>
+        <th>Name</th>
+        <th>Year</th>
+        </tr>
+        <tr>
+            <td>Donald Trump</td>
+            <td>2017</td>
+            </tr>
+            <tr>
+                <td>Barack Obama</td>
+                <td>2009</td>
+                </tr>
+                <tr>
+                    <td>George Bush</td>
+                    <td>2005</td>
+                    </tr>
+            </table>
+</body>
+</html>
