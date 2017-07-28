9d598568289b8baf5cab63147d097dda8c6cd3f3
Nachman Rosen <nachmanrosen@gmail.com>
2017-07-28 14:13:02 -0400

    hw_67

---
+++ hw_67/housesModel.php
@@ -0,0 +1,46 @@
+<?php
+include 'utils/db.php';
+
+if (empty($zip)) {
+    $zip = '';
+}
+
+if (empty($min)) {
+    $min = '';
+}
+
+if (empty($max)) {
+    $max = '';
+}
+
+//if (!empty($_GET['page'])){
+
+//$pagem=$_GET['page'];
+
+//}
+try {
+    $query = "SELECT * FROM houses WHERE (:zip = '' OR zip=:zip)
+                                    AND (:min = '' OR price >= :min)
+                                    AND (:max = '' OR price <= :max)
+                                    LIMIT 4 ";
+ 
+    
+ 
+    $statement = $db->prepare($query);
+    $statement->bindValue('zip', $zip);
+    $statement->bindValue('min', $min);
+    $statement->bindValue('max', $max);
+    if (!empty($_GET['page'])){
+ $query.="OFFSET :start ";
+        $statement->bindValue('start',$_GET['page'] );
+    }
+ 
+
+    
+    $statement->execute();
+    $houses = $statement->fetchAll();
+    $statement->closeCursor();
+} catch (PDOException $e) {
+    $error = "Something went wrong " . $e->getMessage();
+}
+?>
