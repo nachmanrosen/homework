7c23abd11ec9ede3e0e0deb8c27b6b5b2b8fc710
Nachman Rosen <nachmanrosen@gmail.com>
2017-09-14 20:59:33 -0400

    hw_74

---
+++ hw_74/hw1js.js
@@ -0,0 +1,16 @@
+alert(name);
+
+function greaterThan(a,b){
+    if(a>b){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(greaterThan(10,5));
+console.log(greaterThan(5,10));
+console.log(greaterThan(2,5));
+console.log(greaterThan(10,2));
+console.log(greaterThan(1,5));
